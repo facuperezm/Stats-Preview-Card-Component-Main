e10e9d06e7bc3d2db588db9c8d0da01121899276
Facundo Perez Montalvo <facundoperezmontalvo@gmail.com>
2022-08-26 15:54:58 -0300

    done app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import mobile from "./images/image-header-mobile.jpg";
+import desktop from "./images/image-header-desktop.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <section className="section__text">
+          <h1>
+            Get <span>insights</span> that help your business grow.
+          </h1>
+          <p>
+            Discover the benefits of data analytics and make better decisions
+            regarding revenue, customer experience, and overall efficiency.
+          </p>
+          <ul className="list--items">
+            <article>
+              <li>10k+</li>
+              <span>companies</span>
+            </article>
+            <article>
+              <li>314</li>
+              <span>templates</span>
+            </article>
+            <article>
+              <li>12m+</li>
+              <span>queries</span>
+            </article>
+          </ul>
+        </section>
+        <section className="section__img">
+          <div className="img__people">
+            <picture>
+              <source media="(min-width: 768px)" srcSet={desktop} />
+              <img src={mobile} alt="" />
+            </picture>
+            <div className="color"></div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
